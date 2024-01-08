6df048c4b404936b987b1e339d7851cbeaca4219
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-01-08 18:03:57 +0400

    webpack with,css,font,image, map and auto reload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+// import Icon from './icon.png';
+
+const containerSelect = document.querySelector(".container")
+
+let test = document.createElement('h1')
+test.textContent = "testing css!!"
+
+containerSelect.appendChild(test)
