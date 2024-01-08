1ee7700d4d053cffcba3979d467ba269a7ca7f9c
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2024-01-08 18:44:46 +0400

    Prettier instal finished, hopefully and fixed conflict with ESLint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import './style.css';
+import "./style.css";
 // import Icon from './icon.png';
 
-const containerSelect = document.querySelector('.container');
+const containerSelect = document.querySelector(".container");
 
-const test = document.createElement('h1');
-test.textContent = 'testing css!!';
+const test = document.createElement("h1");
+
+test.textContent = "testing css!!";
 
 containerSelect.appendChild(test);
